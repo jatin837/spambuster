de9ca31e70d90dc576ee51679dc61a8b35e67745
dj4t9n <pandeyjatin47@gmail.com>
2021-07-08 16:09:09 +0530

    indentation error

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from helpers import *
 
 if __name__ == "__main__":
-   test_file_path = "./test.txt"
-   raw_contents = get_contents_from_file(test_file_path)
+    test_file_path = "./test.txt"
+    raw_contents = get_contents_from_file(test_file_path)
 
 
