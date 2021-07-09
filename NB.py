ab4693accfc45c7ba111d173e2a60c08047f3334
dj4t9n <pandeyjatin47@gmail.com>
2021-07-09 09:34:45 +0530

    add function signatures

--- NB.py
+++ NB.py
@@ -1,2 +1,14 @@
 def eval_NB(text: str) -> float:
     return 59.00
+
+def get_prior():
+    pass
+
+def get_posterior():
+    pass
+
+def get_likelihood():
+    pass
+
+def get_evidence():
+    pass
