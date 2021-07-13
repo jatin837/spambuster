040ac3230c640352835eaf995cb6f8c70466830b
dj4t9n <pandeyjatin47@gmail.com>
2021-07-14 00:34:50 +0530

    add delay

--- model.py
+++ model.py
@@ -4,6 +4,9 @@ import os
 import numpy as np
 from helpers import get_contents_from_file
 from processors import clean_text
+from time import sleep
+
+DELAY: float = 0.001
 
 class Model(object):
     def __init__(self, path, stop_words="english", max_features=1000, max_df=0.5, min_df=2):
@@ -24,16 +27,23 @@ class Model(object):
         emails = []
         spam_file_path = f'{self.path}/spam/'
         for filename in glob.glob(os.path.join(spam_file_path, '*.txt')):
+            sleep(DELAY)
             emails.append(get_contents_from_file(filename))
+            print(f"...loading {filename}...")
 
         ham_file_path = f'{self.path}/ham/'
         for filename in glob.glob(os.path.join(ham_file_path, '*.txt')):
+            sleep(DELAY)
             emails.append(get_contents_from_file(filename))
+            print(f"...loading {filename}...")
 
         emails_cleaned = []
         for email in emails:
+            sleep(DELAY)
+            print(f"...cleaning...")
             emails_cleaned.append(clean_text(email))
 
+        print(f"...DONE...")
         self.raw_data = emails_cleaned
     
     def get_feature_array(self):
