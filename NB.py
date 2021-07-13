90ae5d0ff3140c90eded3ceb216734d0d9f138b8
dj4t9n <pandeyjatin47@gmail.com>
2021-07-13 12:04:32 +0530

    change the method of computing prior

--- NB.py
+++ NB.py
@@ -8,14 +8,18 @@ labels = np.load("labels.npy")
 def eval_NB(text: str) -> float:
     return 59.00
 
+indx_upto_spam = np.count_nonzero(labels)
+indx_upto_total = labels.shape[0]
+total_spam_features = [np.load(f"data/{str(i + 1)}.npy") for i in range(indx_upto_spam)]
 
-N_spam = labels.count(1)
-N_nspam = labels.count(0)
+N_spam = indx_upto_spam
+N_nspam = indx_upto_total - indx_upto_spam
 
 PRIOR: dict[str, float] = {
         "spam": (N_spam)/(N_spam + N_nspam), 
         "non-spam": (N_nspam)/(N_spam + N_nspam)
         }
+
 
 def get_posterior():
     pass
