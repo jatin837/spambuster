e084c7298a4913f51a76653e3ae9b41c712a890a
dj4t9n <pandeyjatin47@gmail.com>
2021-08-29 12:01:02 +0530

    implement post method template

--- server/api.py
+++ server/api.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+parser = reqparse.RequestParser()
+parser.add_argument('text', type=str, help = 'give me a string of text')
 
-api.add_resource(HelloWorld, '/')
+class Greet(Resource):
+    def get(self):
+        return {'msg': 'hi'}
+
+class TextHandler(Resource):
+    def post(self):
+        args = parser.parse_args()
+        return "I promise to determine if this is a spam or ham", 201
+    
+
+api.add_resource(Greet, '/')
+api.add_resource(TextHandler, '/text')
 
 if __name__ == '__main__':
     print("run ./serv in the root of project")
