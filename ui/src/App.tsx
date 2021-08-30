239d67b09062b8f6fe1d4589a9368f6bcab040e0
dj4t9n <pandeyjatin47@gmail.com>
2021-08-30 15:49:36 +0530

    remove about component from app to Title component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import Title from './components/title';
 import TextBox from './components/textbox';
+import About from './components/about';
 
 const App:React.FC = () => {
   return (
