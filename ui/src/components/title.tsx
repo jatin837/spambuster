ffca3719f3bbcc11822c021d8c0e4342c2316951
dj4t9n <pandeyjatin47@gmail.com>
2021-08-30 15:46:40 +0530

    keep about component in title component, although this looks complete redundent tbh :(

--- ui/src/components/title.tsx
+++ ui/src/components/title.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import About from "./about"
 
 const Title: React.FC = () => {
     return (
         <div>
             <h1>SpamBuster</h1>
+            <About/>
         </div>
     )
 }
