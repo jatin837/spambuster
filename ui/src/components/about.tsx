f48f082f13897502067f3701a224d6d283d41dac
dj4t9n <pandeyjatin47@gmail.com>
2021-08-30 15:48:37 +0530

    add about component

---
+++ ui/src/components/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from 'semantic-ui-react'
+
+const About: React.FC = () => {
+    return (
+        <Container text>
+        <p>
+            A simple spam detector using Naive Bayes Algorithm
+        </p>
+        </Container>
+    )
+}
+
+export default About;
