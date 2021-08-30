dd5a33fb6b7ad07fa60eb3988c8053024a3fd5f2
dj4t9n <pandeyjatin47@gmail.com>
2021-08-30 15:47:24 +0530

    include semantic-ui-react form and button ui element

--- ui/src/components/textbox.tsx
+++ ui/src/components/textbox.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Form, TextArea, Button } from 'semantic-ui-react'
 
-const TextBox: React.FC = () => (
-    <div>
-        <Form>
-            <TextArea rows={25} cols={90} placeholder='enter the text here' />
-        </Form>
-        <Button basic color='violet'>
-            Check now
-        </Button>
-    </div>
-)
+const TextBox: React.FC = () => {
+    const handleSubmit = () => {console.log("submiting, do something about it")}
+    return (
+        <div>
+            <Form onSubmit = {handleSubmit}>
+                <TextArea placeholder='enter the text here' />
+                <Button basic color='violet'>
+                    Check now
+                </Button>
+            </Form>
+        </div>
+    )
+}
 
 export default TextBox;
