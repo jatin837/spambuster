8458ae1d0e9ed9ace1d31f2f77b7a59b6627eb3f
dj4t9n <pandeyjatin47@gmail.com>
2021-09-18 00:11:52 +0530

    implement result component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,13 +2,14 @@ import './App.css';
 import React from "react";
 import Title from './components/title';
 import TextBox from './components/textbox';
-import About from './components/about';
+import Result from './components/result';
 
 const App:React.FC = () => {
   return (
     <div className="App">
       <Title/>
       <TextBox/>
+      <Result/>
     </div>
   );
 }
---
+++ ui/src/components/result.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from 'semantic-ui-react'
+
+const Result: React.FC = () => {
+    return (
+        <Container text>
+        <p>
+        93%
+        </p>
+        </Container>
+    )
+}
+
+export default Result;
