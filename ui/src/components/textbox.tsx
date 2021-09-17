d2880b2648a1d313678ffe3dd0b23871b0d98dfb
dj4t9n <pandeyjatin47@gmail.com>
2021-09-17 23:42:21 +0530

    onChange and onSubmit property added

--- ui/src/components/textbox.tsx
+++ ui/src/components/textbox.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { Form, TextArea, Button } from 'semantic-ui-react'
 
 const TextBox: React.FC = () => {
-    const handleSubmit = () => {console.log("submiting, do something about it")}
+    const [value, setValue] = React.useState('')
+
+    const handleChange = (e:any) => {;
+        console.log(e.target.value);
+		setValue(e.target.value);
+    }
+
+    const handleSubmit = () => {console.log("submiting "+value)}
     return (
         <div>
             <Form onSubmit = {handleSubmit}>
-                <TextArea placeholder='enter the text here' />
+                <TextArea placeholder='enter the text here' onChange={handleChange}/>
                 <Button basic color='violet'>
                     Check now
                 </Button>
