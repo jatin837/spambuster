138e60c0713311b354b185d88550463ebb793b92
dj4t9n <pandeyjatin47@gmail.com>
2021-09-18 15:41:13 +0530

    conditional rendering in result based on result value

--- ui/src/components/result.tsx
+++ ui/src/components/result.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Container } from 'semantic-ui-react'
 
-const Result: React.FC = () => {
-    return (
-        <Container text>
-        <p>
-        93%
-        </p>
-        </Container>
-    )
+interface Props {
+    percentage: Number,
+}
+
+const Result: React.FC<Props> = ({percentage}: Props) => {
+    if (percentage == 0){
+        console.log("Percentage to be determined")
+        return (
+            <div>
+            </div>
+        )
+    }else{
+        return (
+            <Container text>
+                <p>
+                    <b>{percentage}%</b>
+                </p>
+            </Container>
+        )
+    }
 }
 
 export default Result;
