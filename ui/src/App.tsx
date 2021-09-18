e2786eba4ba2d7f5c03d340a2374e64e8763d23d
dj4t9n <pandeyjatin47@gmail.com>
2021-09-18 15:42:10 +0530

    add props callback for updating state of app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -5,11 +5,19 @@ import TextBox from './components/textbox';
 import Result from './components/result';
 
 const App:React.FC = () => {
+  const [val, setVal] = React.useState<String>('')
+  const [percentage, setPercentage] = React.useState(0)
+
+  const evalResult = (val: String) => {
+    setVal(val)
+    console.log("fetch result for "+val)
+  }
+
   return (
     <div className="App">
-      <Title/>
-      <TextBox/>
-      <Result/>
+      <Title />
+      <TextBox evalResult={evalResult}/>
+      <Result percentage = {percentage}/>
     </div>
   );
 }
