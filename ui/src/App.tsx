b78009a74b3ebfbb00e1d0d2f9e7353bb0cd9eb4
dj4t9n <pandeyjatin47@gmail.com>
2021-09-19 15:40:15 +0530

    using axios in App component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,12 +4,18 @@ import Title from './components/title';
 import TextBox from './components/textbox';
 import Result from './components/result';
 
+const axios = require('axios').default;
+
 const App:React.FC = () => {
   const [val, setVal] = React.useState<String>('')
   const [percentage, setPercentage] = React.useState(0)
 
   const evalResult = (val: String) => {
     setVal(val)
+    axios.post('http://localhost:5000/text/', {
+      'text':val
+    }).then( (res:any) => console.log(res) )
+      .catch((err:any) => console.log("error "+err))
     console.log("fetch result for "+val)
   }
 
